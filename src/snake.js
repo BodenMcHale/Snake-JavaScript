bb532bc11daa4cf823a39e3c8090eecb13077f97
Boden McHale <boden.mchale@gmail.com>
2022-06-17 17:51:52 -0400

    Update
    Converted mechanics in game loop to functions
    Added rgb variables for snake and food

--- src/snake.js
+++ src/snake.js
@@ -11,6 +11,9 @@ let canvasGrid = 16;
 // Game loop
 let loopCount = 0;
 
+let foodColor = `rgb(0, 250, 230)`;
+let snakeColor = `rgb(250, 0, 230)`;
+
 let snake = 
 {
     // Initial starting location
@@ -73,24 +76,8 @@ function getRandomInt(min, max)
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// The main game loop
-function game() 
+function wrapSnake()
 {
-  requestAnimationFrame(game);
-
-  // Set the FPS to 15
-  // 60/15 = 4
-  if (loopCount++ < 4) {
-    return;
-  }
-
-  loopCount = 0;
-  context.clearRect(0,0,canvas.width,canvas.height);
-
-  // move snake by it's velocity
-  snake.x += snake.dx;
-  snake.y += snake.dy;
-
   // wrap snake position horizontally on edge of screen
   if (snake.x < 0) {
     snake.x = canvas.width - canvasGrid;
@@ -106,38 +93,54 @@ function game()
   else if (snake.y >= canvas.height) {
     snake.y = 0;
   }
+}
 
-  // keep track of where snake has been. front of the array is always the head
-  snake.cells.unshift({x: snake.x, y: snake.y});
+function drawSnake()
+{
+    // draw snake one cell at a time
+    context.fillStyle = snakeColor;
+    snake.cells.forEach(function(cell, index) 
+    {
+      // drawing 1 px smaller than the canvasGrid creates a canvasGrid effect in the snake body so you can see how long it is
+      context.fillRect(cell.x, cell.y, canvasGrid-1, canvasGrid-1);
+    });
+}
 
-  // remove cells as we move away from them
-  if (snake.cells.length > snake.maxCells) {
-    snake.cells.pop();
-  }
+function moveSnake()
+{
+  // move snake by it's velocity
+  snake.x += snake.dx;
+  snake.y += snake.dy;
+}
 
+function drawFood()
+{
   // draw food
-  context.fillStyle = 'red';
+  context.fillStyle = foodColor;
   context.fillRect(food.x, food.y, canvasGrid-1, canvasGrid-1);
+}
 
-  // draw snake one cell at a time
-  context.fillStyle = 'green';
-  snake.cells.forEach(function(cell, index) {
-
-    // drawing 1 px smaller than the canvasGrid creates a canvasGrid effect in the snake body so you can see how long it is
-    context.fillRect(cell.x, cell.y, canvasGrid-1, canvasGrid-1);
-
-    // snake ate food
-    if (cell.x === food.x && cell.y === food.y) {
+function feedSnake()
+{
+  snake.cells.forEach(function(cell, index) 
+  {
+    if (cell.x === food.x && cell.y === food.y) 
+    {
       snake.maxCells++;
 
       // canvas is 400x400 which is 25x25 grids
       food.x = getRandomInt(0, 25) * canvasGrid;
       food.y = getRandomInt(0, 25) * canvasGrid;
     }
+  });
+}
 
+function collisions()
+{
+  snake.cells.forEach(function(cell, index) 
+  {
     // check collision with all cells after this one (modified bubble sort)
     for (let i = index + 1; i < snake.cells.length; i++) {
-
       // snake occupies same space as a body part. reset game
       if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
         snake.x = 160;
@@ -154,34 +157,71 @@ function game()
   });
 }
 
-// listen to keyboard events to move the snake
-document.addEventListener('keydown', function(e) {
-  // prevent snake from backtracking on itself by checking that it's
-  // not already moving on the same axis (pressing left while moving
-  // left won't do anything, and pressing right while moving left
-  // shouldn't let you collide with your own body)
+function controls()
+{
+  // listen to keyboard events to move the snake
+  document.addEventListener('keydown', function(e) 
+  {
+    // prevent snake from backtracking on itself by checking that it's
+    // not already moving on the same axis (pressing left while moving
+    // left won't do anything, and pressing right while moving left
+    // shouldn't let you collide with your own body)
 
-  // left arrow key
-  if (e.which === 37 && snake.dx === 0) {
-    snake.dx = -canvasGrid;
-    snake.dy = 0;
+    // left arrow key
+    if (e.which === 37 && snake.dx === 0) {
+      snake.dx = -canvasGrid;
+      snake.dy = 0;
+    }
+    // up arrow key
+    else if (e.which === 38 && snake.dy === 0) {
+      snake.dy = -canvasGrid;
+      snake.dx = 0;
+    }
+    // right arrow key
+    else if (e.which === 39 && snake.dx === 0) {
+      snake.dx = canvasGrid;
+      snake.dy = 0;
+    }
+    // down arrow key
+    else if (e.which === 40 && snake.dy === 0) {
+      snake.dy = canvasGrid;
+      snake.dx = 0;
+    }
+  });
+
+}
+
+function game() 
+{
+  requestAnimationFrame(game);
+
+  // Set the FPS to 15
+  // 60/15 = 4
+  if (loopCount++ < 4) {
+    return;
   }
-  // up arrow key
-  else if (e.which === 38 && snake.dy === 0) {
-    snake.dy = -canvasGrid;
-    snake.dx = 0;
+
+  loopCount = 0;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawFood();
+  drawSnake();
+  moveSnake();
+  wrapSnake();
+  feedSnake();
+  collisions();
+
+  // keep track of where snake has been. front of the array is always the head
+  snake.cells.unshift({x: snake.x, y: snake.y});
+
+  // remove cells as we move away from them
+  if (snake.cells.length > snake.maxCells) {
+    snake.cells.pop();
   }
-  // right arrow key
-  else if (e.which === 39 && snake.dx === 0) {
-    snake.dx = canvasGrid;
-    snake.dy = 0;
-  }
-  // down arrow key
-  else if (e.which === 40 && snake.dy === 0) {
-    snake.dy = canvasGrid;
-    snake.dx = 0;
-  }
-});
+
+}
+
+controls();
 
 // start the game
 requestAnimationFrame(game);
