158059dc519c6115059d6a27ec145c03ca4ec5db
Boden McHale <boden.mchale@gmail.com>
2022-06-18 00:05:32 -0400

    Update
    - Added Score
    - Added High Score
    - Added Lives
    - Head of snake is different color than body
    - Added favicon
    - Removed wrap mechanic

--- src/snake.js
+++ src/snake.js
@@ -1,3 +1,44 @@
+/*
+    License
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    MIT License
+    Copyright (c) 2022, Boden McHale
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+	
+    Future Modifications
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  	- SFX
+    - Score
+    - High Score
+    - Convert FPS counter to function, halfway done
+    - Add difficulty levels which are different speeds
+    - Remove looping mechanic
+    - Add thicker yellow border to favicon
+    - Add mute button
+    - Bug: Sometimes food doesn't spawn
+
+    Author
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Twitter: @Boden_McHale https://twitter.com/Boden_McHale
+    Blog: https://lostrabbitdigital.com/blog/
+    Last Updated: June 16th 2022
+*/
+
+
 let canvas = document.getElementById('game');
 let context = canvas.getContext('2d');
 
@@ -13,7 +54,10 @@ let loopCount = 0;
 let isPaused = false;
 
 let foodColor = `rgb(0, 250, 230)`;
-let snakeColor = `rgb(250, 0, 230)`;
+let snakeBodyColor = `rgb(150, 0, 150)`;
+let snakeHeadColor = `rgb(250, 0, 250)`;
+// Border around Snake and Food
+let pixelOffset = 2;
 
 let score = 0;
 let maxScore = score;
@@ -22,8 +66,8 @@ let lives = 3;
 let snake = 
 {
     // Initial starting location
-    x: randomXPositionOnGrid(),
-    y: randomYPositionOnGrid(),
+    x: getRandomXPositionOnGrid(),
+    y: getRandomYPositionOnGrid(),
 
     // Snake velociry
     dx: canvasGrid,
@@ -34,17 +78,17 @@ let snake =
 
     // Length of the snake
     // Set this here for the inital length
-    maxCells: 2
+    maxCells: 1
 };
 
 let food = 
 {
-    x: randomXPositionOnGrid(),
-    y: randomYPositionOnGrid()
+    x: getRandomXPositionOnGrid(),
+    y: getRandomYPositionOnGrid()
 };
 
 // Gets a random X position that is divisible by the canvasGrid
-function randomXPositionOnGrid()
+function getRandomXPositionOnGrid()
 {
     ranPosX = Math.floor(Math.random() * ((canvas.width) - 0)) + 0;
 
@@ -57,7 +101,7 @@ function randomXPositionOnGrid()
 }
 
 // Gets a random Y position that is divisible by the canvasGrid
-function randomYPositionOnGrid()
+function getRandomYPositionOnGrid()
 {
     ranPosX = Math.floor(Math.random() * ((canvas.width) - 0)) + 0;
 
@@ -69,11 +113,6 @@ function randomYPositionOnGrid()
     return ranPosX;
 }
 
-// Get a random whole number within a specific range
-function getRandomInt(min, max) 
-{
-    return Math.floor(Math.random() * (max - min)) + min;
-}
 
 // Get a random whole number within a specific range
 function getRandomInt(min, max) 
@@ -108,49 +147,126 @@ function playSFXMusic()
 
 function wrapSnake()
 {
-  // wrap snake position horizontally on edge of screen
-  if (snake.x < 0) {
+  // Wrap the position of the Snake horizontally
+  if (snake.x < 0) 
+  {
     snake.x = canvas.width - canvasGrid;
   }
-  else if (snake.x >= canvas.width) {
+  else if (snake.x >= canvas.width) 
+  {
     snake.x = 0;
   }
 
-  // wrap snake position vertically on edge of screen
-  if (snake.y < 0) {
+  // Wrap the position of the Snake vertically
+  if (snake.y < 0) 
+  {
     snake.y = canvas.height - canvasGrid;
   }
-  else if (snake.y >= canvas.height) {
+  else if (snake.y >= canvas.height) 
+  {
     snake.y = 0;
+  }
+}
+
+function doNotWrapSnake()
+{
+
+  if (snake.x < 0) 
+  {
+    snake.x = 160;
+    snake.y = 160;
+    snake.cells = [];
+    snake.maxCells = 1;
+    snake.dx = canvasGrid;
+    snake.dy = 0;
+    lives--;
+    score = 0;
+
+    spawnFood();
+  }
+  else if (snake.x >= canvas.width) 
+  {
+    snake.x = 160;
+    snake.y = 160;
+    snake.cells = [];
+    snake.maxCells = 1;
+    snake.dx = canvasGrid;
+    snake.dy = 0;
+    lives--;
+    score = 0;
+
+    spawnFood();
+  }
+
+  if (snake.y < 0) 
+  {
+    snake.x = 160;
+    snake.y = 160;
+    snake.cells = [];
+    snake.maxCells = 1;
+    snake.dx = canvasGrid;
+    snake.dy = 0;
+    lives--;
+    score = 0;
+
+    spawnFood();
+  }
+  else if (snake.y >= canvas.height) 
+  {
+    snake.x = 160;
+    snake.y = 160;
+    snake.cells = [];
+    snake.maxCells = 1;
+    snake.dx = canvasGrid;
+    snake.dy = 0;
+    lives--;
+    score = 0;
+  
+    spawnFood();
   }
 }
 
 function drawUI()
 {
-  maxScore = document.getElementById('max_score').innerHTML;
+  document.getElementById('max_score').innerHTML = maxScore;
+  document.getElementById('score').innerHTML = score;
+  document.getElementById('lives').innerHTML = lives;
 }
 
 function drawFood()
 {
   // draw food
   context.fillStyle = foodColor;
-  context.fillRect(food.x, food.y, canvasGrid-1, canvasGrid-1);
+  context.fillRect(food.x, food.y, canvasGrid-pixelOffset, canvasGrid-pixelOffset);
+}
+
+function spawnFood()
+{
+  food.x = getRandomInt(0, 25) * canvasGrid;
+  food.y = getRandomInt(0, 25) * canvasGrid;
 }
 
 function drawSnake()
 {
-    // draw snake one cell at a time
-    context.fillStyle = snakeColor;
+    // Draw one cell of the Snake at a time
     snake.cells.forEach(function(cell, index) 
     {
-      // drawing 1 px smaller than the canvasGrid creates a canvasGrid effect in the snake body so you can see how long it is
-      context.fillRect(cell.x, cell.y, canvasGrid-1, canvasGrid-1);
+      context.fillStyle = snakeBodyColor;
+      // Draw the body - pixelOffset to see a border around each cell
+      context.fillRect(cell.x, cell.y, canvasGrid-pixelOffset, canvasGrid-pixelOffset);
+      
+      for (let i = index; i < 1; i++) 
+      {
+        context.fillStyle = snakeHeadColor;
+        // Draw the head - pixelOffset to see a border around each cell
+        // This layers another rectangle on top of the body
+        context.fillRect(cell.x, cell.y, canvasGrid - pixelOffset, canvasGrid - pixelOffset);
+      }
     });
 }
 
 function moveSnake()
 {
-  // move snake by it's velocity
   snake.x += snake.dx;
   snake.y += snake.dy;
 }
@@ -162,10 +278,9 @@ function feedSnake()
     if (cell.x === food.x && cell.y === food.y) 
     {
       snake.maxCells++;
+      score++;
 
-      // canvas is 400x400 which is 25x25 grids
-      food.x = getRandomInt(0, 25) * canvasGrid;
-      food.y = getRandomInt(0, 25) * canvasGrid;
+      spawnFood();
     }
   });
 }
@@ -174,19 +289,22 @@ function collisions()
 {
   snake.cells.forEach(function(cell, index) 
   {
-    // check collision with all cells after this one (modified bubble sort)
-    for (let i = index + 1; i < snake.cells.length; i++) {
-      // snake occupies same space as a body part. reset game
-      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+    // Search through each cell, including the snake cell
+    for (let i = index + 1; i < snake.cells.length; i++) 
+    {
+      // If the Snake collides with itself, kill the player
+      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) 
+      {
         snake.x = 160;
         snake.y = 160;
         snake.cells = [];
-        snake.maxCells = 4;
+        snake.maxCells = 1;
         snake.dx = canvasGrid;
         snake.dy = 0;
+        lives--;
+        score = 0;
 
-        food.x = getRandomInt(0, 25) * canvasGrid;
-        food.y = getRandomInt(0, 25) * canvasGrid;
+        spawnFood();
       }
     }
   });
@@ -213,7 +331,7 @@ function controls()
     else if (event.key === 'ArrowDown' && snake.dy == 0 || event.key === 'S' && snake.dy == 0 || event.key === 's' && snake.dy == 0) {
       snake.dy = canvasGrid;
       snake.dx = 0;
-    }
+    } 
     else if (event.key === 'Enter')
     {
       isPaused = !isPaused;
@@ -230,6 +348,9 @@ function game()
 
   // Set the FPS to 15
   // 60/15 = 4
+  let targetFPS = 15;
+  let targetFPSConverted = 60/targetFPS;
+  console.log(targetFPSConverted);
   if (loopCount++ < 4) {
     return;
   }
@@ -238,15 +359,15 @@ function game()
   context.clearRect(0, 0, canvas.width, canvas.height);
 
   maxScore = score > maxScore ? score : maxScore;
-  mute = document.getElementById('muteCanvasCheckbox').checked;
+
   drawUI();
   drawFood();
   drawSnake();
   moveSnake();
-  wrapSnake();
+  //wrapSnake();
+  doNotWrapSnake();
   feedSnake();
   collisions();
-  controls();
 
   // keep track of where snake has been. front of the array is always the head
   snake.cells.unshift({x: snake.x, y: snake.y});
@@ -257,6 +378,9 @@ function game()
     snake.cells.pop();
   }
 }
+
+// Call controls() outside of game() to avoid missing key inputs
+controls();
 
 
 // start the game
